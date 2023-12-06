b59cb4332f6b7ed81795f797aadb092423b45da0
Ryon Tracy <ryon@MP25ZXSM.localdomain>
2023-12-06 07:56:35 -0700

    Finished part 2 day 2, both parts in day2.py

--- Day2/day2.py
+++ Day2/day2.py
@@ -44,6 +44,14 @@ class Game:
     def greens(self):
         return self._colors('green')
 
+    @property
+    def power(self):
+        min_red = max(self.reds)
+        min_blue = max(self.blues)
+        min_green = max(self.greens)
+        power = min_red * min_blue * min_green
+        return power
+
     def is_possible(self, red, blue, green):
         if max(self.reds) > red:
             return False
@@ -60,6 +68,10 @@ for line in lines:
     games.append(game)
 
 possible_games = [game.number for game in games if game.is_possible(red=12, green=13, blue=14)]
-print(possible_games)
-print(sum(possible_games))
+pt1_answer = sum(possible_games)
+print(f'part 1 answer: {pt1_answer}')
+
+powers = [game.power for game in games]
+pt2_answer = sum(powers)
+print(f'part 2 answer {pt2_answer}')
 
